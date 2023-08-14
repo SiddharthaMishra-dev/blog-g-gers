907f0108dc981347567de4369efac11206ae045b
Siddhartha-Mishra <mailbox.siddharthamishra@gmail.com>
2023-08-14 23:32:40 +0530

    user page and blogs page

---
+++ src/app/api/blogs/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest , NextResponse } from "next/server";
+import clientPromise from "@/utils/mongoClient";
+export async function GET(req:NextRequest,res:NextResponse){
+    const client=await clientPromise
+    const db=client.db('Users')
+    const reqRes=await db.collection('blogs').find().toArray()
+    return NextResponse.json(reqRes)
+}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,44 @@
 "use client";
 import { useSession, signOut, signIn } from "next-auth/react";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 const GoogleSignInButton = () => {
   const { data: session } = useSession();
-  console.log(session)
+  console.log(session);
   if (session) {
     return (
       <>
-        <button
-          className="w-1/3 mx-auto flex justify-center bg-slate-600"
-          onClick={() => signOut()}
-        >
+        <div className="w-full h-screen flex justify-center items-center">
+          <div className="flex flex-col gap-2">
+            <Button color="primary" size="lg" onClick={() => signOut()}>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-icon="google"
+                className="mr-8 w-5"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path
+                  fill="red"
+                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                ></path>
+              </svg>
+              SignOut
+            </Button>
+            <Link href={`/protected/${session.user?.name}`}>
+              <Button color="success">Your Blog</Button>
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <Button color="primary" size="lg" onClick={() => signIn("google")}>
           <svg
             aria-hidden="true"
             focusable="false"
@@ -24,32 +53,10 @@ const GoogleSignInButton = () => {
               d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
             ></path>
           </svg>
-          SignOut
-        </button>
-      </>
-    );
-  }
-  return (
-    <button
-      className="w-1/3 mx-auto flex justify-center bg-slate-600"
-      onClick={() => signIn("google")}
-    >
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        data-icon="google"
-        className="mr-8 w-5"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 488 512"
-      >
-        <path
-          fill="red"
-          d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-        ></path>
-      </svg>
-      Continue with Google
-    </button>
+          Continue with Google
+        </Button>
+      </div>
+    </>
   );
 };
 
