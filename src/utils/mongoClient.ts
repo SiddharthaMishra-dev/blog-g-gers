f2aea18fda9bd5d5ac80beb8f80e9118156783d5
SiddharthaMishra-dev <mailbox.siddharthamishra@gmail.com>
2023-10-14 11:33:19 +0530

    type check

--- src/utils/mongoClient.ts
+++ src/utils/mongoClient.ts
@@ -1,13 +1,13 @@
 import { MongoClient } from "mongodb";
 
 // const uri = "mongodb://127.0.0.1:27017/Users";
-const uri = process.env.MONGO_URI || "";
+const uri = process.env.MONGO_URI;
 const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
 
-client = new MongoClient(uri, options);
+client = new MongoClient(uri!, options);
 clientPromise = client.connect();
 
 export default clientPromise;
