6973dc843f1e2ff7047ca3d5de863e5c03db9d2e
SiddharthaMishra-dev <mailbox.siddharthamishra@gmail.com>
2023-10-14 16:31:40 +0530

    fix

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "@/utils/mongoClient";
 export async function GET(req: NextRequest, res: NextResponse) {
   const client = await clientPromise;
-  console.log(process.env.MONGO_URI);
   const db = client.db("Users");
   const reqRes = await db.collection("blogs").find().toArray();
   return NextResponse.json(reqRes);
